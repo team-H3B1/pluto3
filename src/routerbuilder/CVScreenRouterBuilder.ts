f122342957ab98be9d9a15d688ea51797c758c33
Minhyeok Park <opensource@pmh.codes>
2023-05-05 12:02:04 +0900

    feat: add cv alert router

--- src/routerbuilder/CVScreenRouterBuilder.ts
+++ src/routerbuilder/CVScreenRouterBuilder.ts
@@ -1,6 +1,7 @@
-import { Router } from 'express'
+import { type Request, type Response, Router, json } from 'express'
 import type RouterBuilder from './RouterBuilder'
 import { MjpegProxy } from 'mjpeg-proxy'
+import FirebaseService from '../services/FirebaseService'
 
 const {
   AI_BACKEND_URL = 'http://example.com'
@@ -8,9 +9,26 @@ const {
 
 class CVScreenRouterBuilder implements RouterBuilder {
   private readonly router = Router()
+  private readonly firebaseService = new FirebaseService()
 
   constructor () {
+    this.router.use(json())
     this.router.get('/cv.jpg', new MjpegProxy(`${AI_BACKEND_URL}/cv.jpg`).proxyRequest)
+    this.router.post('/', this.sendCVAlert.bind(this))
+  }
+
+  private sendCVAlert (req: Request, res: Response): void {
+    void (async () => {
+      const { message } = req.body
+
+      if (message === undefined) {
+        res.send({ success: false })
+        return
+      }
+
+      await this.firebaseService.sendMessage('안전 경고!', message)
+      res.send({ success: true })
+    })()
   }
 
   public build (): Router {
